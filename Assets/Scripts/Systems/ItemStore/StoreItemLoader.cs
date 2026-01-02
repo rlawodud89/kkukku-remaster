dc0b148f768d0e7b6289ec95b25ac418e44e21bb
KIM JAE YEONG <110236609+rlawodud89@users.noreply.github.com>
2026-01-03 02:47:41 +0900

    feat: 신성 재료 카테고리 선택 버튼 기능 추가
    실, 솜, 달조각 버튼을 누르면 해당하는 Content가 스크롤뷰에 표시

--- Assets/Scripts/Systems/ItemStore/StoreItemLoader.cs
+++ Assets/Scripts/Systems/ItemStore/StoreItemLoader.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StoreItemLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public ScrollRect ScrollView;
+
+    [Header("신성 재료")]
+    public RectTransform YarnContent;
+    public RectTransform CottonContent;
+    public RectTransform MoonContent;
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClickYarnBtn()
     {
-        
+        ScrollView.content = YarnContent;
+
+        YarnContent.gameObject.SetActive(true);
+        CottonContent.gameObject.SetActive(false);
+        MoonContent.gameObject.SetActive(false);
+    }
+
+    public void OnClickCottonBtn()
+    {
+        ScrollView.content = CottonContent;
+
+        YarnContent.gameObject.SetActive(false);
+        CottonContent.gameObject.SetActive(true);
+        MoonContent.gameObject.SetActive(false);
+    }
+
+    public void OnClickMoonBtn()
+    {
+        ScrollView.content = MoonContent;
+
+        YarnContent.gameObject.SetActive(false);
+        CottonContent.gameObject.SetActive(false);
+        MoonContent.gameObject.SetActive(true);
     }
 }
