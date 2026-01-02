e45b6cae6ff6a56195d5ec3b24d26c1b8e21eb92
KIM JAE YEONG <110236609+rlawodud89@users.noreply.github.com>
2026-01-03 02:19:08 +0900

    feat: 메인 메뉴 버튼 기능 추가

---
+++ Assets/Scripts/Systems/ItemStore/MenuClick.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuClick : MonoBehaviour
+{
+    [Header("´Þºû¾ð´ö ÆÐ³Î")]
+    public GameObject MainMenuPanel;
+    public GameObject MaterialPurchasePanel;
+    public GameObject WorkerEmployPanel;
+
+    [Header("±¸¸Å ÆË¾÷Ã¢")]
+    public BuyPopup buyPopup;
+
+    public void OnClickMaterialBtn()
+    {
+        MainMenuPanel.SetActive(false);
+        MaterialPurchasePanel.SetActive(true);
+    }
+
+    public void OnClickLevelBtn()
+    {
+        buyPopup.gameObject.SetActive(true);
+    }
+
+    public void OnClickWorkerBtn()
+    {
+        MainMenuPanel.SetActive(false);
+        MaterialPurchasePanel.SetActive(true);
+    }
+}
