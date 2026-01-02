089d768d961027f726e41edc6a052b7faa37688f
KIM JAE YEONG <110236609+rlawodud89@users.noreply.github.com>
2026-01-03 03:00:19 +0900

    feat: 구매 팝업창 아니오 버튼 기능 추가

--- Assets/Scripts/Systems/ItemStore/BuyPopup.cs
+++ Assets/Scripts/Systems/ItemStore/BuyPopup.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BuyPopup : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TMP_Text InfoText;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClickYesBtn()
     {
-        
+
+    }
+
+    public void OnClickNoBtn()
+    {
+        gameObject.SetActive(false);
     }
 }
