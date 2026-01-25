5c1c053cbdab32637643d976e41d1269456b81cc
KIM JAE YEONG <110236609+rlawodud89@users.noreply.github.com>
2026-01-25 16:18:25 +0900

    chore: Addressable 설치 및 테스트 빌드 세팅
    - 현재는 윈도우 기준으로 빌드 파일 세팅 (테스트 편의 위해)
    - 추후 모바일로 플랫폼 변경해서 새로 빌드 파일 생성 필요

--- Assets/Scripts/Core/DataSystems/Aggregates/ShopStateAggregate.cs
+++ Assets/Scripts/Core/DataSystems/Aggregates/ShopStateAggregate.cs
@@ -1,11 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Security.Cryptography;
-using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
 
 
 public class ShopStateAggregate : IAggregate
