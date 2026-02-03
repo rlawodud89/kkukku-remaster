8397139f474a619795542f7d53580ab730fb77cd
Lee Chae Eun <chaeeun8837@kw.ac.kr>
2026-02-03 16:09:39 +0900

    feat: 퀘스트,편지DB연결

---
+++ Assets/Scripts/Systems/Letter/LetterManager.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LetterManager : MonoBehaviour
+{
+
+    public List<LetterDataSO> allLetterDatas; // 모든 편지 원본 데이터 (SO)
+    List<int> myLetterIDs= new List<int>();
+    public Transform contentPanel;           // 편지 슬롯이 생성될 부모 패널
+    public GameObject letterButtonPrefab;
+
+    public GameObject letterContentPanel; // 편지지 패널
+    private static LetterManager _instance;
+    
+    public static LetterManager Instance
+    {
+        get
+        {
+            // 씬에 생성된 싱글톤이 없으면 자동 생성
+            if (_instance == null)
+            {
+                var obj = new GameObject("GameManager");
+                _instance = obj.AddComponent<LetterManager>();
+                DontDestroyOnLoad(obj);
+            }
+            return _instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as LetterManager;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        allLetterDatas=Resources.LoadAll<LetterDataSO>("ScriptableObjects/Letter").ToList();
+
+
+        
+    }
+
+    private void Start()
+    {
+        // 테스트용
+        //GiveLetter(2);
+        LoadLettersFromDB();
+    }
+
+    
+
+    // 편지 부여
+    public void GiveLetter(int letterID)
+    {
+        myLetterIDs.Add(letterID);
+
+        List<int> list = new List<int>();
+        list.Add(letterID);
+        ServiceLocator.Get<GameData>().Quest.AddLetters(list);
+
+        UpdateUI();
+    }
+
+    public void UpdateUI()
+    {
+        if (contentPanel == null) return;
+
+        //myLetterIDs = ServiceLocator.Get<GameData>().Quest.GetCurrentLetters();
+
+        foreach (int id in myLetterIDs)
+        {
+            // 원본 데이터 찾기
+            LetterDataSO data = allLetterDatas.FirstOrDefault(x => x.letterID == id);
+
+            if (data != null)
+            {
+                // UI 슬롯 생성
+                GameObject slot = Instantiate(letterButtonPrefab, contentPanel);
+                // 슬롯 스크립트에 데이터 전달 (Setup 함수 필요)
+                LetterSlotUI letterSlotUIScript=slot.GetComponent<LetterSlotUI>();
+                letterSlotUIScript.Setup(data);
+
+                Button btn = slot.GetComponent<Button>();
+                btn.onClick.AddListener(() => OpenLetter(data));
+            }
+        }
+    }
+
+    public void LoadLettersFromDB()
+    {
+        // 기존 UI와 데이터 리스트 초기화
+        foreach (Transform child in contentPanel)
+        {
+            Destroy(child.gameObject);
+        }
+
+        //myLetterIDs.Clear();
+
+        // DB에서 현재 보유 중인 편지 ID 리스트 가져오기
+        myLetterIDs = ServiceLocator.Get<GameData>().Quest.GetCurrentLetters();
+
+        if (myLetterIDs == null || myLetterIDs.Count == 0)
+        {
+            Debug.Log("보유 중인 편지가 없습니다.");
+            return;
+        }
+
+        UpdateUI();
+        Debug.Log($"{myLetterIDs.Count}개의 편지를 성공적으로 불러왔습니다.");
+    }
+
+    public void OpenLetter(LetterDataSO letter)
+    {
+        letterContentPanel.SetActive(true);
+
+        letterContentPanel.GetComponent<LetterContentUI>().Setup(letter);
+    }
+}
--- Assets/Scripts/Systems/Quest/QuestDataSO.cs
+++ Assets/Scripts/Systems/Quest/QuestDataSO.cs
@@ -2,14 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public struct Reward
+{
+    public RewardType type; // 보상 종류 (Enum)
+    public int amount;      // 보상 양
+}
+
+public enum RewardType
+{
+    Gold,
+    MoonRock,
+    Energy
+}
+
+
+// 퀘스트 종류 (수집, 대화, 방문 등)
+public enum QuestType
+{
+    CollectBlanket, // 아이템 수집 
+    Fishing,   // 낚시
+    Gathering,   // 채집
+    VisitLocation,   // 장소 방문 
+    TalkToNPC     // NPC 대화
+}
+
+
 [CreateAssetMenu(fileName = "New Quest", menuName = "Quest/Quest Data")]
 public class QuestDataSO : ScriptableObject
 {
     [Header("기본 정보")]
     public int questID;             // 퀘스트 고유 번호 
     public string title;            // 퀘스트 제목
-    [TextArea] public string description; // 설명 
-    public Sprite icon;             // UI에 띄울 아이콘
+    //[TextArea] public string description; // 설명 
+    //public Sprite icon;             // UI에 띄울 아이콘
 
     [Header("난이도 설정")]
     public int requiredLevel;       // 이 퀘스트가 등장할 최소 레벨
@@ -19,7 +45,6 @@ public class QuestDataSO : ScriptableObject
     public int goalCount;           // 목표 수치
 
     [Header("보상")]
-    public int rewardGold;          // 보상 골드
-    public int rewardMoonRock;           // 보상 월석
-    public int rewardEnergy;            // 보상 포근 에너지
+    public Reward reward1;
+    public Reward reward2;
 }
