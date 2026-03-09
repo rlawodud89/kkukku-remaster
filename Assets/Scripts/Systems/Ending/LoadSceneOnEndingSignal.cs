98bda7a8742dd22100cf955bfd0deca3a7aa44ec
KIM JAE YEONG <110236609+rlawodud89@users.noreply.github.com>
2026-03-09 22:39:16 +0900

    fix: 프롤로그 엔딩 충돌 해결

--- Assets/Scripts/Systems/Ending/LoadSceneOnEndingSignal.cs
+++ Assets/Scripts/Systems/Ending/LoadSceneOnEndingSignal.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoadSceneOnSignal : MonoBehaviour
+public class LoadSceneOnEndingSignal : MonoBehaviour
 {
     [SerializeField] private string nextSceneName = "Work_Shop";
 
