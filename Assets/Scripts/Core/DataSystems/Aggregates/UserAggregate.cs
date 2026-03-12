892a85791dacde9d4a39be9855683c8636bd1a76
KIM JAE YEONG <110236609+rlawodud89@users.noreply.github.com>
2026-03-13 01:35:02 +0900

    feat: 가게 크기 관련 메서드 추가

--- Assets/Scripts/Core/DataSystems/Aggregates/UserAggregate.cs
+++ Assets/Scripts/Core/DataSystems/Aggregates/UserAggregate.cs
@@ -20,8 +20,8 @@ public class UserAggregate : IAggregate
 
     // === 기타 데이터 ===
 
-    private Dictionary<int, int> interiorLevelInventoryCount = new Dictionary<int, int>();
-    private Dictionary<int, (int x, int y)> shopLevelSize = new Dictionary<int, (int x, int y)>();
+    private Dictionary<int, int> interiorLevelInventoryCount = new();
+    private Dictionary<int, (int width, int height, Vector3 startPos)> shopLevelSize = new();
 
 
     // === 저장 시스템 사용 메서드 ===
@@ -108,9 +108,15 @@ public class UserAggregate : IAggregate
         interiorLevelInventoryCount.Add(2, 30);
         interiorLevelInventoryCount.Add(3, 40);
 
-        shopLevelSize.Add(1, (10, 6));
-        shopLevelSize.Add(2, (14, 7));
-        shopLevelSize.Add(3, (16, 8));
+        shopLevelSize.Add(1,
+            (10, 6, new Vector3 { x = -5, y = 1 })
+            );
+        shopLevelSize.Add(2,
+            (14, 7, new Vector3 { x = -7, y = 1 })
+            );
+        shopLevelSize.Add(3,
+            (16, 8, new Vector3 { x = -8, y = 2 })
+            );
     }
 
 
@@ -180,10 +186,14 @@ public class UserAggregate : IAggregate
         return (user.interiorInventoryLevel, interiorLevelInventoryCount[user.interiorInventoryLevel]);
     }
 
-    public (int level, (int x, int y) size) GetShopLevel()
+    public (int level, int width, int height) GetShopLevel()
     {
-        return (user.shopLevel, shopLevelSize[user.shopLevel]);
+        return (user.shopLevel,
+            shopLevelSize[user.shopLevel].width,
+            shopLevelSize[user.shopLevel].height
+            );
     }
+
 
     public void ChangeItemShopLevel(int amount)
     {
@@ -255,10 +265,17 @@ public class UserAggregate : IAggregate
         MarkDirty();
     }
 
-    public void GetCurrentShopGridSize(out int x, out int y)
+    public void GetCurrentShopGridSize(out int width, out int height)
     {
-        x = shopLevelSize[user.shopLevel].x;
-        y = shopLevelSize[user.shopLevel].y;
+        width = shopLevelSize[user.shopLevel].width;
+        height = shopLevelSize[user.shopLevel].height;
+    }
+
+    public void GetCurrentShopGridSize(out int width, out int height, out Vector3 startPos)
+    {
+        width = shopLevelSize[user.shopLevel].width;
+        height = shopLevelSize[user.shopLevel].height;
+        startPos = shopLevelSize[user.shopLevel].startPos;
     }
 
     public StartStateType GetStartState()
