fd41217479ffcdc479d6561ad2a55a3568f3d5fc
KIM JAE YEONG <110236609+rlawodud89@users.noreply.github.com>
2026-03-27 15:07:22 +0900

    feat: 시간에 따라 배경 변경 시스템

--- Assets/Scripts/Core/Managers/SaveSystem.cs
+++ Assets/Scripts/Core/Managers/SaveSystem.cs
@@ -7,19 +7,29 @@ using UnityEngine;
 public class SaveSystem : MonoBehaviour
 {
     private SaveService saveService;
-    private static string testdbPath = Path.Combine(Application.streamingAssetsPath, "kkukkuDB.db");
+    private string dbPath;
+    private string testdbPath;
 
     void Awake()
     {
         DontDestroyOnLoad(this);
 
+        dbPath = Path.Combine(Application.persistentDataPath, "kkukkuDB.db");
+        testdbPath = Path.Combine(Application.streamingAssetsPath, "kkukkuDB.db");
+
         // TODO: 모바일 환경에 맞춘 dbPath로 변경 필요
+        bool isNewUser = !File.Exists(testdbPath);
+
         var connection = new SQLiteConnection(testdbPath);
         var repository = new SaveRepository(connection);
         var dirtyRegistry = new DirtyDataRegistry();
 
-        // TODO: 새로운 유저인 경우, 초기 데이터 DB 생성
-        // repository.MakeDefaultDB();
+        // 신규 유저 처리
+        if (isNewUser)
+        {
+            Debug.Log("신규 유저 - DB 생성");
+            repository.MakeDefaultDB();
+        }
 
         var gameData = repository.LoadAll();
 
@@ -30,8 +40,12 @@ public class SaveSystem : MonoBehaviour
         ServiceLocator.Register(dirtyRegistry);
         ServiceLocator.Register(gameData);
 
-        // TODO: 새로운 유저인 경우, 판매 아이템 데이터 생성
-        // gameData.Store.ResetAllStoreItemList();
+        if (isNewUser)
+        {
+            Debug.Log("초기 데이터 세팅");
+            gameData.Store.ResetAllStoreItemList();
+            saveService.SaveNow();
+        }
     }
 
     void Update()
