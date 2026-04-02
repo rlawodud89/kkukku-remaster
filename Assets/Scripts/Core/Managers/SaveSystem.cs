75c719e03a26d1e0b75adec9421d21d9b3ebecee
KIM JAE YEONG <110236609+rlawodud89@users.noreply.github.com>
2026-04-02 21:23:03 +0900

    fix: DB 경로 설정

--- Assets/Scripts/Core/Managers/SaveSystem.cs
+++ Assets/Scripts/Core/Managers/SaveSystem.cs
@@ -13,9 +13,8 @@ public class SaveSystem : MonoBehaviour
     {
         DontDestroyOnLoad(this);
 
-        dbPath = Path.Combine(Application.persistentDataPath, "kkukkuDB.db");
-
-        // TODO: 모바일 환경에 맞춘 dbPath로 변경 필요
+        dbPath = Path.Combine(Application.streamingAssetsPath, "kkukkuDB.db");
+        
         bool isNewUser = !File.Exists(dbPath);
 
         var connection = new SQLiteConnection(dbPath);
